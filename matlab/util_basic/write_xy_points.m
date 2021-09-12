be43c1a7a74103224a66819daf2fe0f59828f6d7
Carl Tape <carltape@gmail.com>
2021-09-12 07:39:17 -0800

    output suffix from dat to txt

--- matlab/util_basic/write_xy_points.m
+++ matlab/util_basic/write_xy_points.m
@@ -8,7 +8,7 @@ if ~exist('stfmt','var')
     stfmt = '%18.10e%18.10e';
 end
 
-filename = [filetag '_points.dat'];
+filename = [filetag '_points.txt'];
 disp(sprintf('writing %i points to file %s',n,filename));
 fid = fopen(filename,'w');
 for ii = 1:n
